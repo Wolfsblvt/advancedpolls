52495efc23501b92e7a016a14fdc62745d7b7686
Raphaël M. <Galixte@users.noreply.github.com>
2015-07-16 13:44:05 +0200

    Major update of FRENCH translation for Advanced Polls extension

---
+++ language/fr/advancedpolls_common.php
@@ -0,0 +1,44 @@
+<?php
+/**
+*
+* Advanced Polls extension for the phpBB Forum Software package.
+* French translation by Galixte (http://www.galixte.com)
+*
+* @copyright (c) 2015 Clemens Husung (Wolfsblvt) <www.pinkes-forum.de>
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+//
+// Some characters you may want to copy&paste:
+// ’ « » “ ” …
+//
+
+$lang = array_merge($lang, array(
+	'NOTIFICATION_AP_POLL_ENDED'		=> '<strong>Les résultats d’un sondage sont disponibles</strong> :',
+	'NOTIFICATION_TYPE_AP_POLL_ENDED'	=> 'Les résulats d’un sondage auquel vous avez participé sont disponibles',
+));
